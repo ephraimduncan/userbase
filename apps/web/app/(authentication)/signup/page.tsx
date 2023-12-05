eee143bc80b4494f0cfe22080ceff4fc55c93db6
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-12-05 08:19:51 +0000

    fix: remove unused files

--- apps/web/app/(authentication)/signup/page.tsx
+++ apps/web/app/(authentication)/signup/page.tsx
@@ -1,39 +1,20 @@
-import { trpcServer } from "@userbase/trpc/react/server";
-import { getServerAuthSession } from "@userbase/lib/auth";
 import { SignIn } from "./signup";
 import { Card } from "@userbase/ui/primitives/card";
 import Link from "next/link";
 
 export default async function Home() {
-  const data = await trpcServer.hello.get.query();
-  const id = await trpcServer.hello.getId.query({ text: "world" });
-
-  const session = await getServerAuthSession();
-
   return (
     <div className="h-screen">
-      {session && (
-        <div className="text-2xl p-4">
-          <h1 className="text-2xl">Hello world</h1>
+      <div className="p-5 flex flex-col mx-auto items-center justify-center h-full">
+        <SignIn />
 
-          {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-          {data && <pre>{JSON.stringify(id, null, 2)}</pre>}
-          {data && <pre>{JSON.stringify(session, null, 2)}</pre>}
-        </div>
-      )}
-
-      {!session && (
-        <div className="p-5 flex flex-col mx-auto items-center justify-center h-full">
-          <SignIn />
-
-          <Card className="mt-8 py-3 px-8 text-sm rounded-full">
-            Already have an account?{" "}
-            <Link href="/signin">
-              <span className="text-primary font-medium">Sign in</span>
-            </Link>
-          </Card>
-        </div>
-      )}
+        <Card className="mt-8 py-3 px-8 text-sm rounded-full">
+          Already have an account?{" "}
+          <Link href="/signin">
+            <span className="text-primary font-medium">Sign in</span>
+          </Link>
+        </Card>
+      </div>
     </div>
   );
 }
